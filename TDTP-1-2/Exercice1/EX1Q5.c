f0f8e67b701a77b006b3ed0edad5ccee4b23e1bd
MAX BOD <98185988+maxkzime@users.noreply.github.com>
2022-09-11 21:47:33 +0200

    Exercice 5.

--- TDTP-1-2/Exercice1/EX1Q5.c
+++ TDTP-1-2/Exercice1/EX1Q5.c
@@ -14,95 +14,33 @@ int main(int argc, char* argv[])
     // Valeur de retour.
     int returnValue = 0;
 
-    if(argc > 1)
+
+    // Ouverture du fichier à lire.
+    int df = open("./res.data", O_RDONLY);
+
+    if(df != -1)
     {
-        for (int i = 1; i < argc; i++)
+        lseek(df, 30, SEEK_SET);
+
+        int charRead = 0;
+
+        char buf[1];
+        int lu = 1;
+
+        while(lu != 0 && charRead < 10)
         {
-            /*** Ajout par rapport à la commande cat -n ***/
-            // Réinitialisation pour chaque fichier du nombre de lignes.
-            lNb = 0;
+            lu = read(df, buf, 1);
+            write(1, buf, lu);
 
-            printf("\n\n");
-            /*** ^^^ Ajouts par rapport à la commande cat -n ^^^ ***/
-
-            // Ouverture du fichier à lire.
-            int df = open(argv[i], O_RDONLY);
-
-            if(df != -1)
-            {
-                IncrementLine();
-                printf("%s", lineNb);
-
-                char buf[1];
-
-                int lu = read(df, buf, 1);
-
-                write(1, buf, lu);
-
-                while(lu != 0)
-                {
-                    lu = read(df, buf, 1);
-
-                    write(1, buf, lu);
-
-                    if(buf[0] == '\n')
-                    {
-                        IncrementLine();
-                        printf("%s", lineNb);
-                    }
-                }
-
-                close(df);
-            }
-            else
-            {
-                //cat: error.txt: No such file or directory
-                printf("Impossible d'ouvrir le fichier : %s\n", argv[i]);
-                returnValue = 1;
-            }
+            charRead++;
         }
 
+        close(df);
     }
-    else if(argc == 1)
+    else
     {
-        char charBuf[1];
-        charBuf[0] = '\0';
-
-        char wordBuf[256];
-        int letterVal = 0;
-
-        int lu;
-        int octSize = 0;
-
-        while(1)
-        {
-            lu = read(0, charBuf, 1);
-
-            if(charBuf[0] != '\0')
-            {
-                if(charBuf[0] == '\n')
-                {
-                    IncrementLine();
-                    printf("%s", lineNb);
-
-                    write(1, wordBuf, octSize);
-                    printf("\n");
-
-                    letterVal = 0;
-
-                    memset(wordBuf, 0, 255);
-                }
-                else
-                {
-                    octSize += lu;
-
-                    wordBuf[letterVal] = charBuf[0];
-                    letterVal++;
-                }
-            }
-        }
+        returnValue = 1;
     }
-
 
     return(returnValue);
 }
